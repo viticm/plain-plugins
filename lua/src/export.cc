64cb733d2899b1be2e11824f066108315e35b5a7
viticm <viticm@126.com>
2022-01-14 20:16:31 +0800

    Change the plugin open functions

--- lua/src/export.cc
+++ lua/src/export.cc
@@ -1,4 +1,5 @@
 #include "pf/plugin/lua/export.h"
+#include "pf/engine/kernel.h"
 #include "pf/plugin/lua/system.h"
 
 pf_script::Interface *env_creator() {
@@ -6,9 +7,10 @@ pf_script::Interface *env_creator() {
 }
 
 extern "C"
-PF_PLUGIN_API void pfopen_pf_plugin_lua(void *_params) {
+PF_PLUGIN_API void 
+pfopen_pf_plugin_lua(pf_engine::Kernel *engine, void *_params) {
   auto params = *cast(const pf_basic::type::variable_array_t *, _params);
   if (params.size() <= 0) return;
   auto type = params[0].get<int8_t>();
-  register_env_creator_script(type, env_creator);
+  engine->register_script(type, env_creator);
 }
--- protocol/src/export.cc
+++ protocol/src/export.cc
@@ -1,13 +1,15 @@
+#include "pf/engine/kernel.h"
 #include "pf/net/protocol/helpers.h"
-#include "pf/plugin/protocol/export.h"
 #include "pf/plugin/protocol/tlbb.h"
+#include "pf/plugin/protocol/export.h"
 
 extern "C"
-PF_PLUGIN_API void pfopen_pf_plugin_protocol(void *_params) {
+PF_PLUGIN_API void
+pfopen_pf_plugin_protocol(pf_engine::Kernel *engine, void *_params) {
   using namespace pf_plugin::protocol;
   auto params = *cast(const pf_basic::type::variable_array_t *, _params);
   auto dynamic = params.size() > 0 ? params[0].get<bool>() : false;
   static TLBB tlbb;
   tlbb.set_dynamic(dynamic);
-  pf_net::protocol::set("tlbb", &tlbb);
+  engine->register_protocol("tlbb", &tlbb);
 }
