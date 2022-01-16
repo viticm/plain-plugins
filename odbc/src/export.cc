b311c035cf01188501b755089b99f20db4c64a2b
viticm <viticm@126.com>
2022-01-16 13:10:47 +0800

    Revert export apis

--- odbc/src/export.cc
+++ odbc/src/export.cc
@@ -1,5 +1,6 @@
 #include "pf/db/config.h"
 #include "pf/plugin/odbc/system.h"
+#include "pf/engine/kernel.h"
 #include "pf/plugin/odbc/export.h"
 
 pf_db::Interface *env_creator() {
@@ -8,8 +9,8 @@ pf_db::Interface *env_creator() {
 
 extern "C"
 PF_PLUGIN_API void
-pfopen_pf_plugin_odbc(void *_params) {
+pfopen_pf_plugin_odbc(pf_engine::Kernel *engine, void *_params) {
   auto params = *cast(const pf_basic::type::variable_array_t *, _params);
   auto type = (uint8_t)kDBEnvODBC;
-  register_env_creator_db(type, env_creator);
+  engine->register_db(type, env_creator);
 }
--- protocol/src/export.cc
+++ protocol/src/export.cc
@@ -4,11 +4,11 @@
 
 extern "C"
 PF_PLUGIN_API void
-pfopen_pf_plugin_protocol(void *_params) {
+pfopen_pf_plugin_protocol(pf_engine::Kernel *engine, void *_params) {
   using namespace pf_plugin::protocol;
   auto params = *cast(const pf_basic::type::variable_array_t *, _params);
   auto dynamic = params.size() > 0 ? params[0].get<bool>() : false;
   static TLBB tlbb;
   tlbb.set_dynamic(dynamic);
-  pf_net::protocol::set("tlbb", &tlbb);
+  engine->register_protocol("tlbb", &tlbb);
 }
